90db0843e68de566e4252e6d022b1506fa71454c
Vaibhav Matere <vpm00718897@gmail.com>
2023-05-31 13:04:51 +0530

    refactor : removed about page

--- src/App.vue
+++ src/App.vue
@@ -11,9 +11,6 @@
       <router-link to="/past-orders" class="top-bar-link">
         <span>Past Orders</span>
       </router-link>
-      <router-link to="/about" class="top-bar-link">
-        <span>About</span>
-      </router-link>
     </nav>
     <!-- <router-link @click="toggleSidebar" class="top-bar-cart-link">
       <i class="icofont-cart-alt icofont-1x"></i>
